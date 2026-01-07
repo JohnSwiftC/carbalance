6a9eceb64d13c01201188ea42b6d0233117bfb44
John Swift <johnswift@live.com>
2026-01-07 18:00:46 +0000

    Initial commit; Ordering impl on road stretches

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+pub mod route;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/route.rs
@@ -0,0 +1,47 @@
+use std::cmp::{Eq, Ord, PartialEq, PartialOrd};
+
+pub struct Stretch {
+    speed: u32,
+    length: u32,
+    cars: u32,
+    cap: u32,
+}
+
+impl Stretch {
+    fn weight(&self) -> f32 {
+        let lov = self.length as f32 / self.speed as f32;
+        let noc = (self.cars as f32 / self.cap as f32);
+        let noc4 = noc * noc * noc * noc;
+        (0.15 * noc4 + 1.0) * lov
+    }
+}
+
+impl PartialEq for Stretch {
+    fn eq(&self, other: &Self) -> bool {
+        // Im gonna say that two stretches are effectively equal
+        // if they are within some tolerance of eachother, this
+        // may not actually be needed but whatever
+
+        (self.weight() - other.weight()).abs() <= 0.001
+    }
+}
+
+impl Eq for Stretch {}
+
+impl PartialOrd for Stretch {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Stretch {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self == other {
+            std::cmp::Ordering::Equal
+        } else if self.weight() < other.weight() {
+            std::cmp::Ordering::Less
+        } else {
+            std::cmp::Ordering::Greater
+        }
+    }
+}
