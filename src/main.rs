e1aa94a8bfc3bb335af78dd65dd8e6ab63c411b0
JohnSwiftC <johnswift@live.com>
2026-01-09 19:54:06 -0500

    Add: small demo in main

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,44 @@ use route::{Map, Stretch};
 
 fn main() {
     let mut map = Map::with_capacity(15);
+
+    map.push(Stretch {
+        speed: 10,
+        length: 12,
+        cars: 5,
+        cap: 8,
+    });
+    map.push(Stretch {
+        speed: 10,
+        length: 12,
+        cars: 5,
+        cap: 8,
+    });
+    map.push(Stretch {
+        speed: 10,
+        length: 12,
+        cars: 5,
+        cap: 8,
+    });
+    map.push(Stretch {
+        speed: 10,
+        length: 12,
+        cars: 5,
+        cap: 8,
+    });
+    map.push(Stretch {
+        speed: 10,
+        length: 12,
+        cars: 5,
+        cap: 8,
+    });
+
+    map.connect(0, 1);
+    map.connect(1, 2);
+    map.connect(4, 3);
+    map.connect(3, 2);
+
+    let r = map.solve(0, 4);
+
+    println!("{:#?}", r);
 }
